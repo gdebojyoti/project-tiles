fa829d0549d3b68af7a15ec7e73b35c63c8f8a4c
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-02-14 02:31:32 +0530

    Hash enabled in bundled file names.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/app.ts',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle-[name]-[hash:8].js',
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {

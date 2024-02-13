c05f66cc4c4f67aacbf5cc91ae63dc39a8bc13c7
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-02-14 01:44:28 +0530

    Trigger the layout animation during "restart".

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -14,7 +14,7 @@ class Game {
     this._mapData = await this.loadMap(-1)
     console.log("mapData", this._mapData)
 
-    this.initScene()
+    this.initScene(true)
 
     this.initButtons()
 
@@ -30,7 +30,8 @@ class Game {
     return sampleMapData.default
   }
 
-  private initScene (): void {
+  // initialize scene; triggered during first start and game restarts
+  private initScene (isFirstStart: boolean): void {
     const { TILE_SIZE, TILE_GAP } = CONFIG
     const { positionCompensation, cells } = this._mapData
 
@@ -56,8 +57,10 @@ class Game {
     sceneElm.style.width = `${highestCol * (TILE_SIZE + TILE_GAP)}px`
     sceneElm.style.height = `${highestRow * (TILE_SIZE + TILE_GAP)}px`
 
-    // add position compensation to scene's transform property
-    sceneElm.style.transform = `${getComputedStyle(sceneElm).transform} translate(${positionCompensation.x}px, ${positionCompensation.y}px)`
+    // add position compensation to scene's transform property (first time only, and not during "restarts")
+    if (isFirstStart) {
+      sceneElm.style.transform = `${getComputedStyle(sceneElm).transform} translate(${positionCompensation.x}px, ${positionCompensation.y}px)`
+    }
 
     // empty scene element
     sceneElm.innerHTML = ''
@@ -86,7 +89,7 @@ class Game {
             break
           case 'restart':
             // restart the game
-            // this.initScene()
+            this.initScene(false)
             this._engine?.restart()
             break
           case 'undo':

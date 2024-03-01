dbaf9191205cf9a274c5bf06dbc955e74d6b0838
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-03-02 03:04:26 +0530

    Home screen & Menu. (#7)
    * Menu system introduced.
    
    * Cleanup.
    
    * Readme updated.
    
    * Logo minified.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import './styles/main.css'
+import './styles/home.css'
 import './styles/modals.css'
 
 import GameEngine from './components/GameEngine'
+import MenuUiEngine from './components/MenuUiEngine'
 import UiEngine from './components/UiEngine'
 import OverworldUiEngine from './components/OverworldUiEngine'
 
 window.onload = async () => {
   const game = new GameEngine()
-  new UiEngine(game)
+  const menuUi = new MenuUiEngine(game)
+  new UiEngine(game, menuUi)
   new OverworldUiEngine(game)
 }

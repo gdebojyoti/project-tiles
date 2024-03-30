4a4c5281ae5a2d3c21707c60b6d31476f070fe9b
Debojyoti Ghosh <debojyoti.ghosh@housing.com>
2024-03-31 03:42:53 +0530

    File rename & refactor.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 
-import Logo from 'app/components/home/Logo'
-import Nav from 'app/components/home/Nav'
-import Footer from 'app/components/home/Footer'
+import Landing from 'app/components/home/Landing'
 
-import styles from './page.module.css'
+// import styles from './page.module.css'
 
 const Home = () => {
   return (
-    <div id="home" className={styles.home}>
-      <Logo />
-      <Nav />
-      <Footer />
-    </div>
+    <Landing />
   )
 }
 
